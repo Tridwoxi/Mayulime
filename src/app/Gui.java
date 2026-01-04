2cbe5310afb3277a7aba8f7a69503c3278230735
Tridwoxi <dw.oxidane@gmail.com>
2026-01-04 00:47:56 -0500

    Add gui.

--- src/app/Gui.java
+++ src/app/Gui.java
@@ -1,26 +1,75 @@
 package app;
 
+import java.io.File;
+import javafx.geometry.Insets;
 import javafx.scene.Group;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
+import javafx.stage.Window;
 import think.representation.Board;
 import think.representation.Cell;
 
-public class Gui {
+class Gui extends VBox {
 
-    public static final double CELL_SIZE = 24.0; // Pixels.
+    private static final double CELL_SIZE = 20.0; // Pixels.
+    private static final double PADDING = 50.0;
+    private static final double SPACING = 50.0;
+
+    private BoardDisplay boardDisplay;
+    private final Text scoreDisplay;
+    private final Text rubberDisplay;
+
+    public Gui() {
+        super(SPACING);
+        this.boardDisplay = new BoardDisplay();
+        this.scoreDisplay = new Text();
+        this.rubberDisplay = new Text();
+        setPadding(new Insets(PADDING));
+        getChildren().addAll(
+            boardDisplay, // Must be at position 0 to match with setBoard(Board).
+            new HBox(SPACING, scoreDisplay, rubberDisplay, makeButton())
+        );
+    }
+
+    public void setBoard(Board board) {
+        boardDisplay = new BoardDisplay(board, CELL_SIZE);
+        getChildren().set(0, boardDisplay);
+    }
+
+    private Button makeButton() {
+        Button upload = new Button("Upload problem");
+        upload.setOnAction(event -> {
+            FileChooser chooser = new FileChooser();
+            chooser
+                .getExtensionFilters()
+                .add(new ExtensionFilter("Pathery level specification", "*.txt"));
+            Window active = getScene() == null ? null : getScene().getWindow();
+            File chosen = chooser.showOpenDialog(active);
+            if (chosen != null) {
+                Main.solveProblem(chosen);
+            }
+        });
+        return upload;
+    }
 }
 
 class BoardDisplay extends Group {
 
-    public BoardDisplay(Board board) {
+    public BoardDisplay() {}
+
+    public BoardDisplay(Board board, double cellSize) {
         for (int i = 0; i < board.getBoundI(); i++) {
             for (int j = 0; j < board.getBoundJ(); j++) {
-                CellDisplay cell = new CellDisplay(board.getCell(i, j));
-                cell.setLayoutX(j * Gui.CELL_SIZE);
-                cell.setLayoutY(i * Gui.CELL_SIZE);
+                CellDisplay cell = new CellDisplay(board.getCell(i, j), cellSize);
+                cell.setLayoutX(j * cellSize);
+                cell.setLayoutY(i * cellSize);
                 getChildren().add(cell);
             }
         }
@@ -29,8 +78,8 @@ class BoardDisplay extends Group {
 
 class CellDisplay extends Group {
 
-    public CellDisplay(Cell cell) {
-        Rectangle rect = new Rectangle(Gui.CELL_SIZE, Gui.CELL_SIZE);
+    public CellDisplay(Cell cell, double cellSize) {
+        Rectangle rect = new Rectangle(cellSize, cellSize);
         rect.setFill(getColor(cell));
         rect.setStroke(Color.DARKGRAY);
 
@@ -40,9 +89,9 @@ class CellDisplay extends Group {
             cell.type() == Cell.CellType.TELEPORT_OUT
         ) {
             Text label = new Text(String.valueOf(cell.association()));
-            label.setFont(Font.font(Gui.CELL_SIZE * 0.5));
-            label.setX((Gui.CELL_SIZE - label.getLayoutBounds().getWidth()) * 0.5);
-            label.setY((Gui.CELL_SIZE + label.getLayoutBounds().getHeight()) * 0.5);
+            label.setFont(Font.font(cellSize * 0.5));
+            label.setX((cellSize - label.getLayoutBounds().getWidth()) * 0.5);
+            label.setY((cellSize + label.getLayoutBounds().getHeight()) * 0.5);
             getChildren().addAll(rect, label);
         } else {
             getChildren().add(rect);
--- src/app/Main.java
+++ src/app/Main.java
@@ -1,13 +1,23 @@
 package app;
 
+import java.io.File;
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public final class Main extends Application {
 
+    private Gui gui;
+
     @Override
     public void start(final Stage primaryStage) {
+        gui = new Gui();
         primaryStage.setTitle("henlo");
+        primaryStage.setScene(new Scene(gui));
         primaryStage.show();
+    }
+
+    public static void solveProblem(File file) {
+        throw new UnsupportedOperationException();
     }
 }
