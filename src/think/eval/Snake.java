c121b24f685d61a835225830990293f9db6c7ee3
Tridwoxi <dw.oxidane@gmail.com>
2026-01-04 13:42:32 -0500

    Add snake template

---
+++ src/think/eval/Snake.java
@@ -0,0 +1,18 @@
+package think.eval;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import think.repr.Board;
+import think.repr.Point;
+
+public class Snake {
+
+    public static ArrayList<Point> bfs(
+        Board board,
+        HashSet<Point> rubbers,
+        HashSet<Point> destinations,
+        Point source
+    ) {
+        throw new UnsupportedOperationException();
+    }
+}
