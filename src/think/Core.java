4ea24f4c8adf995dfd82111f759e0bbb196c8deb
Tridwoxi <dw.oxidane@gmail.com>
2026-01-04 01:24:26 -0500

    Wire in empty core.

--- src/think/Core.java
+++ src/think/Core.java
@@ -1,8 +1,11 @@
 package think;
 
+import app.Main;
 import think.representation.Board;
 
 public class Core {
 
-    public Core(Board board) {}
+    public Core(Board board) {
+        Main.recieveUpdate(board, 0, board.getMaxRubbers());
+    }
 }
