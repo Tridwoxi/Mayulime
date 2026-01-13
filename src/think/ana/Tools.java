202ce8595a5797c168f074aaf3805f51a8d25d6f
Tridwoxi <dw.oxidane@gmail.com>
2026-01-13 03:06:58 -0500

    Add lazy random stream.

--- src/think/ana/Tools.java
+++ src/think/ana/Tools.java
@@ -2,6 +2,8 @@ package think.ana;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.Spliterators;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
@@ -36,6 +38,41 @@ public final class Tools {
         }
         assert result.size() == size;
         return result;
+    }
+
+    // == Randoms. =====================================================================
+
+    /**
+        Lazily stream items in a random order. Does not modify underyling list, but is
+        sensitive to changes in it.
+     */
+    public static <T> Stream<T> randomly(final ArrayList<T> items) {
+        final int size = items.size();
+        final int[] indices = new int[size];
+        for (int i = 0; i < size; i++) {
+            indices[i] = i;
+        }
+        final Iterator<T> iterator = new Iterator<>() {
+            private int remaining = size;
+
+            @Override
+            public boolean hasNext() {
+                return remaining > 0;
+            }
+
+            @Override
+            public T next() {
+                final int choice = ThreadLocalRandom.current().nextInt(remaining);
+                final int index = indices[choice];
+                remaining -= 1;
+                indices[choice] = indices[remaining];
+                return items.get(index);
+            }
+        };
+        return StreamSupport.stream(
+            Spliterators.spliteratorUnknownSize(iterator, 0),
+            false
+        );
     }
 
     // == Pairwise. ====================================================================
