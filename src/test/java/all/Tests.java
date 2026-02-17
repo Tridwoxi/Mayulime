67b640c798883e91683fdb2e73a5713c31dcd646
Tridwoxi <dw.oxidane@gmail.com>
2026-02-17 01:27:35 -0500

    test(all): use provided junit assertions

--- src/test/java/all/Tests.java
+++ src/test/java/all/Tests.java
@@ -1,12 +1,12 @@
 package all;
 
-import infra.io.Logging;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.BiFunction;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import think.ana.Snake;
 import think.repr.Grid;
@@ -18,10 +18,10 @@ import think.tools.Random;
 import think.tools.Structures.Weighted;
 
 /**
-    Testing of the system is done by unit tests (below) and assertions in the code.
-    Assertions are used for both conditions and invariants. Tests are used for
-    end-to-end behavior and running the assertions. Do not write tests that are already
-    covered by assertions.
+    Testing of the system is done with the unit tests below.
+
+    Currently, the system uses a mix of tests and assertions. I plan to migrate
+    assertions into unit tests.
  */
 public final class Tests {
 
@@ -29,30 +29,19 @@ public final class Tests {
         6.4.13.Example...:7,c2.,r1.,u2.,t1.,r1.,u1.,t2.,r1.3,s1.1,r1.,c1.1,f1.
         """;
 
-    /**
-        Death by failed assertion if any test fails.
-     */
-    @Test
-    public void runAllTests() {
-        Logging.log(Tests.class, "Running unit tests");
-        problemParsing();
-        snakePathfinding();
-        snakeTiebreaking();
-        solutionValidation();
-        weightedSelection();
-    }
-
     // == think.ana ====================================================================
 
-    private static void snakePathfinding() {
+    @Test
+    public void snakeEvaluation() {
         final Problem problem = getProblem();
-        assert Snake.evaluate(problem, problem.getCachedInitial()) == 30;
+        Assertions.assertEquals(30, Snake.evaluate(problem, problem.getCachedInitial()));
         final Grid<Feature> solution = problem.getAnotherInitial();
         solution.set(new Cell(0, 3), Feature.PLAYER_WALL);
-        assert Snake.evaluate(problem, solution) == 0;
+        Assertions.assertEquals(0, Snake.evaluate(problem, solution));
     }
 
-    private static void snakeTiebreaking() {
+    @Test
+    public void snakeTiebreaking() {
         final BiFunction<Cell, Cell, Cell> first2x2 = (start, end) -> {
             final Optional<ArrayList<Cell>> steps = Snake.travel(
                 new Grid<>(Feature.EMPTY, 2, 2),
@@ -67,68 +56,73 @@ public final class Tests {
         final Cell bottomRight = new Cell(1, 1);
         final Cell topRight = new Cell(0, 1);
         final Cell bottomLeft = new Cell(1, 0);
-        assert first2x2.apply(topLeft, bottomRight).equals(topRight);
-        assert first2x2.apply(bottomLeft, topRight).equals(topLeft);
-        assert first2x2.apply(bottomRight, topLeft).equals(topRight);
-        assert first2x2.apply(topRight, bottomLeft).equals(bottomRight);
+        Assertions.assertEquals(topRight, first2x2.apply(topLeft, bottomRight));
+        Assertions.assertEquals(topLeft, first2x2.apply(bottomLeft, topRight));
+        Assertions.assertEquals(topRight, first2x2.apply(bottomRight, topLeft));
+        Assertions.assertEquals(bottomRight, first2x2.apply(topRight, bottomLeft));
     }
 
     // == think.repr ===================================================================
 
-    private static void problemParsing() {
+    @Test
+    public void parseProblem() {
         final Problem problem = getProblem();
 
         final Grid<Feature> initial = problem.getCachedInitial();
-        assert initial.getNumRows() == 4;
-        assert initial.getNumCols() == 6;
-        assert problem.getPlayerWallSupply() == 12;
-        assert problem.getName().equals("Example");
+        Assertions.assertEquals(4, initial.getNumRows());
+        Assertions.assertEquals(6, initial.getNumCols());
+        Assertions.assertEquals(12, problem.getPlayerWallSupply());
+        Assertions.assertEquals("Example", problem.getName());
 
         final ArrayList<Cell> checkpoints = problem.getCheckpoints();
-        assert checkpoints.size() == 4;
-        assert checkpoints.get(0).equals(new Cell(3, 0));
-        assert checkpoints.get(1).equals(new Cell(3, 3));
-        assert checkpoints.get(2).equals(new Cell(1, 1));
-        assert checkpoints.get(3).equals(new Cell(3, 5));
+        Assertions.assertEquals(4, checkpoints.size());
+        Assertions.assertEquals(new Cell(3, 0), checkpoints.get(0));
+        Assertions.assertEquals(new Cell(3, 3), checkpoints.get(1));
+        Assertions.assertEquals(new Cell(1, 1), checkpoints.get(2));
+        Assertions.assertEquals(new Cell(3, 5), checkpoints.get(3));
 
         final HashMap<Cell, Cell> teleports = problem.getTeleports();
-        assert teleports.size() == 2;
-        assert teleports.get(new Cell(1, 4)).equals(new Cell(2, 0));
-        assert teleports.get(new Cell(2, 1)).equals(new Cell(1, 3));
+        Assertions.assertEquals(2, teleports.size());
+        Assertions.assertEquals(new Cell(2, 0), teleports.get(new Cell(1, 4)));
+        Assertions.assertEquals(new Cell(1, 3), teleports.get(new Cell(2, 1)));
     }
 
-    private static void solutionValidation() {
+    @Test
+    public void solutionValidation() {
         final Problem problem = getProblem();
         final Grid<Feature> solution = problem.getAnotherInitial();
 
-        assert problem.isValid(solution);
+        Assertions.assertTrue(problem.isValid(solution));
 
         solution.set(new Cell(0, 0), Feature.SYSTEM_WALL);
-        assert !problem.isValid(solution);
+        Assertions.assertFalse(problem.isValid(solution));
         solution.set(new Cell(0, 0), Feature.EMPTY);
 
         solution.set(new Cell(3, 3), Feature.TELEPORT_OUT);
-        assert !problem.isValid(solution);
+        Assertions.assertFalse(problem.isValid(solution));
         solution.set(new Cell(3, 3), Feature.CHECKPOINT);
 
         solution.set(new Cell(0, 2), Feature.PLAYER_WALL);
-        assert problem.isValid(solution);
+        Assertions.assertTrue(problem.isValid(solution));
         solution.set(new Cell(3, 3), Feature.EMPTY);
     }
 
     // == think.tools ==================================================================
 
-    private static void weightedSelection() {
+    @Test
+    public void weightedSelection() {
         final List<Weighted<String>> weighteds = List.of(
             new Weighted<>("Light", 1e-10),
             new Weighted<>("Heavy", 1e+10),
             new Weighted<>("Light", 1e-10)
         );
         for (int attempt = 0; attempt < 10; attempt += 1) {
-            assert Random.weightedStream(new ArrayList<>(weighteds))
-                .findFirst()
-                .orElseThrow()
-                .equals("Heavy");
+            Assertions.assertEquals(
+                "Heavy",
+                Random.weightedStream(new ArrayList<>(weighteds))
+                    .findFirst()
+                    .orElseThrow()
+            );
         }
     }
 
@@ -138,6 +132,7 @@ public final class Tests {
         try {
             return new Problem(EXAMPLE);
         } catch (BadMapCodeException exception) {
+            Assertions.fail(exception);
             throw new AssertionError();
         }
     }
