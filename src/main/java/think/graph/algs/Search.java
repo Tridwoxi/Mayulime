06741f35f4c9ea876e65f332af30a61788e146ca
Tridwoxi <dw.oxidane@gmail.com>
2026-02-26 18:27:28 -0500

    refactor(graph): clean up search fill

--- src/main/java/think/graph/algs/Search.java
+++ src/main/java/think/graph/algs/Search.java
@@ -7,14 +7,13 @@ import java.util.Deque;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Predicate;
 import think.graph.Graph;
 
 /**
     Search algorithms upon a graph. The returned {@link Fill} parent maps of the search algorithms
-    always reflects the order of children exploration. For example, on a graph with equal weights,
-    "Start -> A, B; A -> Finish; B -> Finish;" then the path from Start to Finish will always be
-    "Start -> A -> Finish" and never "Start -> B -> Finish".
+    always reflects the order of children exploration. For example, on a graph with equal weights
+    "Start -> A, B; A -> Finish; B -> Finish;", the path from Start to Finish will always be "Start
+    -> A -> Finish" and never "Start -> B -> Finish".
  */
 public final class Search {
 
@@ -45,24 +44,31 @@ public final class Search {
      */
     public static final class Fill<K, V, E> {
 
-        private final Map<K, K> toParent; // get(K) -> parent of K.
-        private final Map<K, E> fromParent; // get(K) -> edge from parent of K to K.
-        private final Map<K, V> toValue; // get(K) -> value of K.
+        private final Map<K, K> parents; // get(K) -> parent of K.
+        private final Map<K, E> edges; // get(K) -> edge from parent of K to K.
+        private final Map<K, V> values; // get(K) -> value of K.
         private final K source;
 
         public Fill(
-            final Map<K, K> toParent,
-            final Map<K, E> fromParent,
-            final Map<K, V> toValue,
+            final Map<K, K> parents,
+            final Map<K, E> edges,
+            final Map<K, V> values,
             final K source
         ) {
-            this.toParent = new HashMap<>(toParent);
-            this.fromParent = new HashMap<>(fromParent);
-            this.toValue = new HashMap<>(toValue);
-            this.source = source;
-            validate(() -> {
+            final boolean properSourcePrescence =
+                parents.get(source) == null &&
+                edges.get(source) == null &&
+                values.get(source) != null;
+            final boolean keySetMatch =
+                parents.keySet().equals(values.keySet()) &&
+                !parents.keySet().equals(edges.keySet());
+            if (!properSourcePrescence || !keySetMatch) {
                 throw new IllegalArgumentException();
-            });
+            }
+            this.parents = new HashMap<>(parents);
+            this.edges = new HashMap<>(edges);
+            this.values = new HashMap<>(values);
+            this.source = source;
         }
 
         public K getSource() {
@@ -70,53 +76,39 @@ public final class Search {
         }
 
         public boolean isReachable(final K key) {
-            return toParent.containsKey(key);
+            return parents.containsKey(key);
         }
 
         public Path<K, V, E> getPathTo(final K reachableVertexKey) {
             if (!isReachable(reachableVertexKey)) {
                 throw new IllegalArgumentException();
             }
-            final List<K> keys = new ArrayList<>();
+            final List<K> pathKeys = new ArrayList<>();
             K walker = reachableVertexKey;
             while (!walker.equals(source)) {
-                keys.add(walker);
-                walker = toParent.get(walker);
+                pathKeys.add(walker);
+                walker = parents.get(walker);
             }
-            keys.add(source);
-            Collections.reverse(keys);
+            pathKeys.add(source);
+            Collections.reverse(pathKeys);
 
-            final List<V> values = keys.stream().map(toValue::get).toList();
-            final List<E> edges = keys.stream().skip(1L).map(fromParent::get).toList();
-            return new Path<>(keys, values, edges);
+            final List<V> pathValues = pathKeys.stream().map(values::get).toList();
+            final List<E> pathEdges = pathKeys.stream().skip(1L).map(edges::get).toList();
+            return new Path<>(pathKeys, pathValues, pathEdges);
         }
 
-        private static <K, V, E> Fill<K, V, E> fromToParentsAndGraph(
-            final Map<K, K> toParent,
+        private static <K, V, E> Fill<K, V, E> fromParentsAndGraph(
+            final Map<K, K> parents,
             final Graph<K, V, E> graph,
             final K source
         ) {
-            final Map<K, E> fromParent = HashMap.newHashMap(toParent.size());
-            final Map<K, V> toValue = HashMap.newHashMap(toParent.size());
-            toParent.forEach((child, parent) -> {
-                toValue.put(child, graph.getVertexValue(child));
-                if (!child.equals(source)) {
-                    fromParent.put(child, graph.getEdge(parent, child));
-                }
+            final Map<K, E> edges = HashMap.newHashMap(parents.size());
+            final Map<K, V> values = HashMap.newHashMap(parents.size());
+            parents.forEach((child, parent) -> {
+                values.put(child, graph.getVertexValue(child));
+                edges.put(child, child.equals(source) ? null : graph.getEdge(parent, child));
             });
-            return new Fill<K, V, E>(toParent, fromParent, toValue, source);
-        }
-
-        private void validate(final Runnable uponFailure) {
-            if (toParent.size() != toValue.size() || toParent.size() != fromParent.size() + 1) {
-                uponFailure.run();
-            }
-            final Predicate<K> okay = vertexKey ->
-                !vertexKey.equals(source) &&
-                (!toParent.containsKey(vertexKey) || !toValue.containsKey(vertexKey));
-            if (!toParent.keySet().stream().allMatch(okay)) {
-                uponFailure.run();
-            }
+            return new Fill<K, V, E>(parents, edges, values, source);
         }
     }
 
@@ -139,6 +131,6 @@ public final class Search {
                 }
             }
         }
-        return Fill.fromToParentsAndGraph(parents, graph, source);
+        return Fill.fromParentsAndGraph(parents, graph, source);
     }
 }
