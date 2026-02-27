83ffde19ecd7bd5291fe60791797e7467dedde8e
Tridwoxi <dw.oxidane@gmail.com>
2026-02-27 01:22:11 -0500

    feat(domain): add puzzle spec

--- src/main/java/think2/domain/repr/Puzzle.java
+++ src/main/java/think2/domain/repr/Puzzle.java
@@ -1,6 +1,43 @@
 package think2.domain.repr;
 
+import java.util.List;
+import think2.graph.impl.GridGraph.Cell;
+
 /**
     Pathery problem specification. Contains metadata and empty board supplier.
  */
-public final class Puzzle {}
+public final class Puzzle {
+
+    private final String name;
+    private final Board original;
+    private final List<Cell> checkpoints;
+    private final int wallBudget;
+
+    public Puzzle(
+        final String name,
+        final Board original,
+        final List<Cell> checkpoints,
+        final int wallBudget
+    ) {
+        this.name = name;
+        this.original = original.shallowCopy();
+        this.checkpoints = List.copyOf(checkpoints);
+        this.wallBudget = wallBudget;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Board getOriginal() {
+        return original.shallowCopy();
+    }
+
+    public List<Cell> getCheckpoints() {
+        return checkpoints;
+    }
+
+    public int getWallBudget() {
+        return wallBudget;
+    }
+}
