0b27851fcd8032eb3ec67427af7efc0968009034
Tridwoxi <dw.oxidane@gmail.com>
2026-02-27 01:48:57 -0500

    feat(solve): copy solver to think2

--- src/main/java/think2/solve/Solver.java
+++ src/main/java/think2/solve/Solver.java
@@ -1,6 +1,76 @@
 package think2.solve;
 
+import infra.io.Logging;
+import think2.domain.repr.Board;
+import think2.domain.repr.Puzzle;
+
 /**
-    Abstract base class for Pathery solvers.
+    Find board configurations to solve Pathery puzzles. This abstract class provides useful getters
+    and a framework to integrate its concrete subclasses with the rest of the system.
  */
-public abstract class Solver {}
+public abstract class Solver implements Runnable {
+
+    @FunctionalInterface
+    public interface SolutionListener {
+        void listen(String submitter, Puzzle puzzle, Board solution);
+    }
+
+    private final SolutionListener listener;
+    private final Puzzle puzzle;
+    private volatile boolean alive;
+
+    public Solver(final SolutionListener listener, final Puzzle puzzle) {
+        this.listener = listener;
+        this.puzzle = puzzle;
+        this.alive = true;
+    }
+
+    // == Public API. =============================================================================
+
+    @Override
+    public final void run() {
+        // When the user instructs us to work on a different problem, we should work on it. But
+        // many solvers run forever, and there is no safe way to forcefully kill a thread or
+        // procedure. So, the solver needs to check when to stop. We can do so with a lengthy chain
+        // of "if not alive, return", but throwing exceptions is an easier way to do non-local
+        // returns.
+        Logging.info("Started %s", getClass().getSimpleName());
+        try {
+            solve();
+            Logging.info("Terminated %s (returned normally)", getClass().getSimpleName());
+        } catch (final KilledException exception) {
+            Logging.info("Terminated %s (killed)", getClass().getSimpleName());
+        }
+    }
+
+    public final void pleaseDie() {
+        this.alive = false;
+    }
+
+    // == Subclass contract. ======================================================================
+
+    /**
+        Concrete subclasses should do all non-trivial work in this method, as opposed to the
+        constructor. Implementations of this method are free to hang indefintely, but it would be
+        nice if this method calls {@link #checkAlive} at least once every 500 miliseconds.
+     */
+    protected abstract void solve() throws KilledException;
+
+    // == Protected API. ==========================================================================
+
+    protected static final class KilledException extends Exception {}
+
+    protected final void checkAlive() throws KilledException {
+        if (!alive) {
+            throw new KilledException();
+        }
+    }
+
+    protected final Puzzle getPuzzle() {
+        return puzzle;
+    }
+
+    protected final SolutionListener getListener() {
+        return listener;
+    }
+}
