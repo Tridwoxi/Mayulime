6f99464bd037629631b52f1c94ea5dc8d90b9676
Tridwoxi <dw.oxidane@gmail.com>
2026-02-28 01:26:06 -0500

    feat(solve): add iteration and binomial tools

--- src/main/java/think2/solve/impl/RandomSolver.java
+++ src/main/java/think2/solve/impl/RandomSolver.java
@@ -1,12 +1,13 @@
 package think2.solve.impl;
 
 import java.util.ArrayList;
-import think.tools.Random;
-import think.tools.Random.RestrictedBinomialDistribution;
+import java.util.Collections;
 import think2.domain.repr.Board;
 import think2.domain.repr.Puzzle;
 import think2.graph.impl.GridGraph.Cell;
 import think2.solve.Solver;
+import think2.solve.tools.Iteration;
+import think2.solve.tools.RestrictedBinomial;
 
 /**
     Guess randomly.
@@ -16,15 +17,12 @@ import think2.solve.Solver;
 public final class RandomSolver extends Solver {
 
     private final ArrayList<Cell> emptyCells;
-    private final RestrictedBinomialDistribution numWalls;
+    private final RestrictedBinomial numWalls;
 
     public RandomSolver(final ProposedSolution listener, final Puzzle puzzle) {
         super(listener, puzzle);
         this.emptyCells = new ArrayList<>(getPuzzle().getOriginal().getOriginallyEmpty());
-        this.numWalls = new RestrictedBinomialDistribution(
-            emptyCells.size(),
-            getPuzzle().getWallBudget()
-        );
+        this.numWalls = new RestrictedBinomial(emptyCells.size(), getPuzzle().getWallBudget());
     }
 
     @Override
@@ -37,7 +35,7 @@ public final class RandomSolver extends Solver {
 
     private Board createRandomBoard() {
         final Board board = getPuzzle().getOriginal();
-        Random.uniformStream(emptyCells).limit(numWalls.sample()).forEachOrdered(board::placeWall);
+        Iteration.randomly(emptyCells).limit(numWalls.sample()).forEach(board::placeWall);
         return board;
     }
 }
---
+++ src/main/java/think2/solve/tools/Iteration.java
@@ -0,0 +1,18 @@
+package think2.solve.tools;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
+
+public final class Iteration {
+
+    private Iteration() {}
+
+    public static <T> Stream<T> randomly(final Collection<T> items) {
+        final List<T> view = new ArrayList<>(items);
+        Collections.shuffle(view);
+        return view.stream();
+    }
+}
