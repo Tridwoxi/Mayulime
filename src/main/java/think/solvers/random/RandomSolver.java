ab8ed692b5bd84e48621008ef8f1fe4052810dac
Tridwoxi <dw.oxidane@gmail.com>
2026-03-05 15:09:52 -0500

    feat(random): add random solver

---
+++ src/main/java/think/solvers/random/RandomSolver.java
@@ -0,0 +1,55 @@
+package think.solvers.random;
+
+import domain.model.Maze;
+import domain.model.Maze.Feature;
+import domain.model.Puzzle;
+import think.common.IntArrays;
+import think.solvers.Solver;
+
+public final class RandomSolver extends Solver {
+
+    private final int[] emptyCellIndices;
+    private final RestrictedBinomial wallDistribution;
+    private final int numRows;
+    private final int numCols;
+
+    public RandomSolver(final ProposedSolution listener, final Puzzle puzzle) {
+        super(listener, puzzle);
+        final Maze maze = puzzle.getMaze();
+        this.emptyCellIndices = getEmptyCellIndices(maze.getGrid());
+        this.wallDistribution = new RestrictedBinomial(
+            puzzle.getBlockingBudget(),
+            emptyCellIndices.length
+        );
+        this.numRows = maze.getNumRows();
+        this.numCols = maze.getNumCols();
+    }
+
+    @Override
+    protected void solve() throws KilledException {
+        while (true) {
+            checkAlive();
+            propose(generateRandomSolution());
+        }
+    }
+
+    private Maze generateRandomSolution() {
+        final Feature[] grid = getPuzzle().getMaze().getGrid();
+        IntArrays.shuffleInPlace(emptyCellIndices);
+
+        final int numWalls = wallDistribution.sample();
+        for (int placement = 0; placement < numWalls; placement += 1) {
+            final int cell = emptyCellIndices[placement];
+            grid[cell] = Feature.SYSTEM_WALL;
+        }
+        return new Maze(grid, numRows, numCols);
+    }
+
+    private static int[] getEmptyCellIndices(final Feature[] features) {
+        final int[] emptyCellIndices = new int[features.length];
+        for (int index = 0; index < features.length; index += 1) {
+            emptyCellIndices[index] = features[index] == Feature.BLANK ? index : -1;
+        }
+        return IntArrays.filteredCopy(emptyCellIndices, -1);
+    }
+}
---
+++ src/main/java/think/solvers/random/RestrictedBinomial.java
@@ -0,0 +1,69 @@
+package think.solvers.random;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+    Sample {@code element} from range {@code 0} (inclusive) to {@code limit} (inclusive) with weight
+    {@code population choose element}. This implementation is approximate.
+ */
+public final class RestrictedBinomial {
+
+    private final ArrayList<Double> cumulativeDistribution;
+
+    public RestrictedBinomial(final int population, final int limit) {
+        if (population < limit) {
+            throw new IllegalArgumentException();
+        }
+        this.cumulativeDistribution = build(population, limit);
+    }
+
+    public int sample() {
+        if (cumulativeDistribution.isEmpty()) {
+            return 0;
+        }
+        final double total = cumulativeDistribution.getLast();
+        // Binary search returns "(-(insertion point) - 1)" if the value is not found. Since a
+        // double has many bits, it will never be found.
+        final int index = -Collections.binarySearch(
+            cumulativeDistribution,
+            ThreadLocalRandom.current().nextDouble() * total
+        );
+        return index - 1;
+    }
+
+    private static ArrayList<Double> build(final int population, final int limit) {
+        final ArrayList<Double> rawLogProbs = new ArrayList<>(limit + 1);
+        for (int element = 0; element <= limit; element += 1) {
+            rawLogProbs.add(logBinom(population, element));
+        }
+        final double largest = rawLogProbs.stream().reduce(0.0, Math::max);
+        final ArrayList<Double> normLogProbs = new ArrayList<>(limit + 1);
+        for (int element = 0; element <= limit; element += 1) {
+            normLogProbs.add(rawLogProbs.get(element) - largest);
+        }
+        final ArrayList<Double> cumulativeDistribution = new ArrayList<>(limit + 1);
+        double sum = 0.0;
+        for (int element = 0; element <= limit; element += 1) {
+            sum += Math.exp(normLogProbs.get(element));
+            cumulativeDistribution.add(sum);
+        }
+        return cumulativeDistribution;
+    }
+
+    private static double logBinom(final int population, final int element) {
+        // There is 1 way to pick none or all of the elements from a population.
+        if (element == 0 || element == population) {
+            return Math.log(1.0);
+        }
+        // A binomial coefficient n choose k is calculated n! / k! (n - k)!. Since Stirling's
+        // approximation works on factorials, it also works on binomial coefficients.
+        // https://math.stackexchange.com/questions/64716/approximating-the-logarithm-of-the-binomial-coefficient
+        return (
+            population * Math.log(population) -
+            element * Math.log(element) -
+            (population - element) * Math.log(population - element)
+        );
+    }
+}
