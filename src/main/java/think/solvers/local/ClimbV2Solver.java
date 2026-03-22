65a12aa46cf32bd24550acac1fd1224d3e3e854a
Tridwoxi <dw.oxidane@gmail.com>
2026-03-22 01:02:21 -0400

    feat(local): prepare shortest path detection

--- src/main/java/think/solvers/local/ClimbV2Solver.java
+++ src/main/java/think/solvers/local/ClimbV2Solver.java
@@ -11,10 +11,12 @@ import think.solvers.Solver;
 public final class ClimbV2Solver extends Solver {
 
     private final int[] initiallyBlankCells;
+    private final int[] checkpoints;
 
     public ClimbV2Solver(final Consumer<Proposal> listener, final Puzzle puzzle) {
         super(listener, puzzle);
         this.initiallyBlankCells = getCellsWhere(puzzle.getFeatures(), Feature.BLANK);
+        this.checkpoints = getPuzzle().getCheckpoints();
     }
 
     @Override
@@ -111,6 +113,15 @@ public final class ClimbV2Solver extends Solver {
             }
         }
         return false;
+    }
+
+    private int[] getCellsOnShortestPath(final Feature[] features) {
+        final boolean[] candidates = new boolean[features.length];
+        for (int index = 0; index < features.length; index += 1) {
+            candidates[index] = features[index].isPassable();
+        }
+        // TODO: dp, pointer list?
+        return IntArrays.ofRangeWhere(0, candidates.length, index -> candidates[index]);
     }
 
     private static int[] getCellsWhere(final Feature[] features, final Feature feature) {
