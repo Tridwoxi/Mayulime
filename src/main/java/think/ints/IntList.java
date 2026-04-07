0e7b424d1ff80c6c789e0fc9e629e554f5aea002
Tridwoxi <dw.oxidane@gmail.com>
2026-04-07 16:48:56 -0400

    feat(ints): begin migration to dedicated package

---
+++ src/main/java/think/ints/IntList.java
@@ -0,0 +1,126 @@
+package think.ints;
+
+import java.util.Arrays;
+import java.util.function.IntConsumer;
+import think.common.IntArrays;
+
+/**
+    Array-backed list of primitive ints.
+ */
+public final class IntList {
+
+    public static final int NOT_FOUND = -1;
+    private int[] backing;
+    private int size;
+
+    public IntList(final int initialCapacity) {
+        if (initialCapacity > 0) {
+            this.backing = new int[initialCapacity];
+        } else if (initialCapacity == 0) {
+            this.backing = IntArrays.EMPTY;
+        } else {
+            throw new IllegalArgumentException("" + initialCapacity);
+        }
+    }
+
+    public IntList(final int[] values) {
+        this(values.length);
+        extend(values);
+    }
+
+    public void add(final int value) {
+        ensureCapacity(size + 1);
+        backing[size] = value;
+        size += 1;
+    }
+
+    public void insert(final int index, final int value) {
+        if (index == size) {
+            add(value);
+            return;
+        }
+        requireInBounds(index);
+        ensureCapacity(size + 1);
+        System.arraycopy(backing, index, backing, index + 1, size - index);
+        backing[index] = value;
+        size += 1;
+    }
+
+    public void extend(final int[] values) {
+        ensureCapacity(size + values.length);
+        System.arraycopy(values, 0, backing, size, values.length);
+        size += values.length;
+    }
+
+    public int get(final int index) {
+        requireInBounds(index);
+        return backing[index];
+    }
+
+    public int set(final int index, final int value) {
+        requireInBounds(index);
+        final int old = backing[index];
+        backing[index] = value;
+        return old;
+    }
+
+    public int removeElement(final int value) {
+        final int index = indexOf(value);
+        if (index == NOT_FOUND) {
+            return NOT_FOUND;
+        }
+        removeAt(index);
+        return index;
+    }
+
+    public int removeAt(final int index) {
+        requireInBounds(index);
+        final int value = backing[index];
+        System.arraycopy(backing, index + 1, backing, index, size - index - 1);
+        size -= 1;
+        return value;
+    }
+
+    public void clear() {
+        size = 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public int indexOf(final int value) {
+        for (int index = 0; index < size; index += 1) {
+            if (backing[index] == value) {
+                return index;
+            }
+        }
+        return NOT_FOUND;
+    }
+
+    public void forEach(final IntConsumer action) {
+        for (int index = 0; index < size; index += 1) {
+            action.accept(backing[index]);
+        }
+    }
+
+    public void ensureCapacity(final int minCapacity) {
+        if (minCapacity <= backing.length) {
+            return;
+        }
+        final int actualCapacity = Math.max(minCapacity, backing.length << 1);
+        final int[] newBacking = new int[actualCapacity];
+        System.arraycopy(backing, 0, newBacking, 0, size);
+        backing = newBacking;
+    }
+
+    public int[] toArray() {
+        return Arrays.copyOf(backing, size);
+    }
+
+    private void requireInBounds(final int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("" + index);
+        }
+    }
+}
