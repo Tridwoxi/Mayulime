4417fc7eba8461f64103c4bb1dd4644b8ca0183c
Tridwoxi <dw.oxidane@gmail.com>
2026-04-07 17:13:50 -0400

    feat(ints): add IntQueue

---
+++ src/main/java/think/ints/IntQueue.java
@@ -0,0 +1,48 @@
+package think.ints;
+
+/**
+    Ring buffer-backed queue of primitve ints. Designed and optimized solely for breath-first
+    search. That means no error checking and fixed size. Illegal operations result in unspecified
+    behavior.
+ */
+public final class IntQueue {
+
+    private int[] backing;
+    private int mask;
+    private int head;
+    private int tail;
+
+    public IntQueue(final int finalCapacity) {
+        // Ring buffers sacrifice one unit of capacity in the backing array to use the head == tail
+        // trick, so we make up for it with an extra element here.
+        int actualCapacity = Integer.highestOneBit(finalCapacity + 1);
+        if (actualCapacity < finalCapacity + 1) {
+            actualCapacity <<= 1;
+        }
+        this.backing = new int[actualCapacity];
+        this.mask = actualCapacity - 1;
+        this.head = 0;
+        this.tail = 0;
+    }
+
+    public void add(final int value) {
+        // PERF: async-profiler says this method is 74% of thread runtime for ClimbSolver.
+        backing[tail] = value;
+        tail = (tail + 1) & mask;
+    }
+
+    public int remove() {
+        final int value = backing[head];
+        head = (head + 1) & mask;
+        return value;
+    }
+
+    public boolean isEmpty() {
+        return head == tail;
+    }
+
+    public void clear() {
+        head = 0;
+        tail = 0;
+    }
+}
